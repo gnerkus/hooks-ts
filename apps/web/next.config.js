103bf6dab4899c45f1bee9fe8b476b8e17f0ccad
gnerkus <ifeanyioraelosi@gmail.com>
2023-07-23 16:34:06 +0200

    exclude 'fs' when building Nextjs web app

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.fallback = {
+
+      // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped.
+      ...config.resolve.fallback,  
+
+      fs: false, // the solution
+    };
+    
+    return config;
+  },
 };
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,15 +1,13 @@
 import "../styles/globals.css";
-// include styles from the ui package
-import "ui/styles.css";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className="bg-zinc-900">
-      <body>{children}</body>
-    </html>
-  );
-}
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    );
+  }
